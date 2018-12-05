f03b4c3e3d32f2d76a9513f4faa9732f1f39899a
Bryson Spaulding <brysonian10@hotmail.com>
2018-12-05 07:59:17 -0700

    I moved around my variables a bit becase the guy next to me keeps looking at my screen to get info instead of paying attention in class because he is on his phone all the time. So I added some extra variables just to confuse him so that he can ask me a question. I'm fine if he asked a question but he keeps looking at my screen to try to catch up. Anyways I put the real code in the comment slashes in ChatPanel.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -5,17 +5,40 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 import chat.controller.ChatController;
 
-public class ChatPanel
+public class ChatPanel extends JPanel
 {
-	private JTextField inputField;
+	private JTextField chatField;
 	private JButton chatButton;
-	private JButton checkerButton;
-	private JButton loadButton;
-	private JButton saveButton;
+	private JButton checkedButton;
+	private JButton loadingButton;
+	private JButton savingButton;
 	private SpringLayout appLayout;
 	private ChatController appController;
 	private JTextArea chatArea;
 	private JScrollPane chatPane;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		savingButton = new JButton("Why");
+		loadingButton = new JButton("Starting loading when ever you need to");
+		chatButton = new JButton("Chat with the robot here");
+		checkedButton = new JButton("Check Text just in case you need to make sure there is nothing bad.");
+		
+		chatField = new JTextField("Talk to the bot here if you need to explain stuff.", 50);
+		chatArea = new JTextArea("Chat Area", 20, 50);
+		
+		chatPane = new JScrollPane();
+		
+		setupScrollPane();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
 	
 	
 	private void setupPanel()
@@ -23,10 +46,7 @@ public class ChatPanel
 		
 	}
 	
-	public ChatPanel(ChatController appController)
-	{
-		
-	}
+	
 	
 	private void setupScrollPane()
 	{
@@ -44,5 +64,9 @@ public class ChatPanel
 		});
 	}
 	
+	private void setupLayout()
+	{
+		
+	}
 	
 }
