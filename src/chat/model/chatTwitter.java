3925348ea6440ace5bd611ab61f38a51d0b051f7
Bryson Spaulding <brysonian10@hotmail.com>
2019-04-23 09:41:14 -0600

    Created the basic framework of the chatTwitter.java file.

--- src/chat/model/chatTwitter.java
+++ src/chat/model/chatTwitter.java
@@ -1,6 +1,13 @@
 package chat.model;
 
+import chat.controller.ChatController;
+
 public class chatTwitter
 {
-
+	private ChatController app;
+	
+	public chatTwitter(ChatController app)
+	{
+		this.app = app;
+	}
 }
